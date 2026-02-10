b9b53e5322921df8348154563ec219ff215e13b8
TMLY12 <yurecmen23@gmail.com>
2026-02-10 17:45:11 +0100

    Test 1

--- Test 1.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-def get_days_from_today(date: str) -> int:
-    try:
-        today = datetime.today().date()
-        target_date = datetime.strptime(date, "%Y-%m-%d").date()
-        return (today-target_date).days
-    except ValueError:
-        print("Wrong format. Enter the date format: YYYY-MM-DD.")
